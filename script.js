096edf6004107685faeaf357520b7123c202db23
shibu-g <109884948+shibu-g@users.noreply.github.com>
2023-02-13 22:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
 
+var btn=document.getElementById('btn');
+var p=document.getElementById('num');
+function f() {
+	let no=Math.floor(Math.random()*20);
+	p.textContent=no;
+}
+btn.addEventListener("click",f);
