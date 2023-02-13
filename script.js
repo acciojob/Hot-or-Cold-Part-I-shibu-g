2c724af5ec1d847d54428cb353b29f08ce560afd
shibu-g <109884948+shibu-g@users.noreply.github.com>
2023-02-13 22:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 
 var btn=document.getElementById('btn');
 var p=document.getElementById('num');
+
+var res=document.getElementById('respond');
+var inp=document.getElementById('guess').value;
+
 function f() {
 	let no=Math.floor(Math.random()*20);
 	p.textContent=no;
+	var inp=document.getElementById('guess').value;
+    if(Number(inp)-no<=5){
+		res.textContent="Hot";
+	}else{
+		res.textContent="Cold";
+	}
 }
 btn.addEventListener("click",f);
